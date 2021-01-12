36abfd463d785e6a3b8669b7ee98e559830f0607
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-01-12 10:12:55 -0600

    initial commit

---
+++ employees/request.py
@@ -0,0 +1,37 @@
+  EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Tom Bisop",
+      "locationId": 1
+    },
+    {
+      "id": 2,
+      "name": "Jon Edward",
+      "locationId": 2
+    },
+    {
+      "id": 3,
+      "name": "Jeremy Bakker",
+      "locationId": 3
+    },
+    {
+      "id": 4,
+      "name": "Herry Parker",
+      "locationId": 2
+    },
+    {
+      "name": "Mango",
+      "locationId": 2,
+      "animalId": 4,
+      "id": 5
+    },
+    {
+      "name": "Hero",
+      "locationId": 2,
+      "animalId": 6,
+      "id": 6
+    }
+  ]
+
+  def get_all_employees():
+      return EMPLOYEES
