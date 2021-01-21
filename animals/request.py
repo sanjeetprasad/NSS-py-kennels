27e1abb736811fa71421ae570f4633c4751e58ff
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2021-01-21 15:27:25 -0600

    clone down the the nashville react template and update the animal by sql update mathod

--- animals/request.py
+++ animals/request.py
@@ -138,14 +138,14 @@ def delete_animal(id):
         WHERE id = ?
         """, (id, ))
 
-def update_animal(id, new_animal):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            # Found the animal. Update the value.
-            ANIMALS[index] = new_animal
-            break
+# def update_animal(id, new_animal):
+#     # Iterate the ANIMALS list, but use enumerate() so that
+#     # you can access the index value of each item.
+#     for index, animal in enumerate(ANIMALS):
+#         if animal["id"] == id:
+#             # Found the animal. Update the value.
+#             ANIMALS[index] = new_animal
+#             break
 
 def get_animals_by_location(location_id):
     with sqlite3.connect("./kennel.db") as conn:
@@ -202,11 +202,30 @@ def get_animals_by_status(status):
 
     return json.dumps(animals)
 
-# def delete_animal(id):
-#     with sqlite3.connect("./kennel.db") as conn:
-#         db_cursor = conn.cursor()
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-#         db_cursor.execute("""
-#         DELETE FROM animal
-#         WHERE id = ?
-#         """, (id, ))            
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
